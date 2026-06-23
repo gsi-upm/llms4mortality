a370518aaac3d87ccd072010b3145d7771443113
Daucco <daucco@gmail.com>
2026-06-23 02:27:33 +0200

    WIP: Data selection for consistency with comparing study.

--- _extra_immuno/dataselect_immuno.py
+++ _extra_immuno/dataselect_immuno.py
@@ -4,29 +4,27 @@
 from pathlib import Path
 import pandas as pd
 import argparse
+import json
 
 PATH_MIMICIV = ''
+FPATH_JSON_IMMUNO = ''
 
 # Lists of relevant ICD9 and 10 codes for immunosuppressed patients. Taken from supplementary material of reference study.
-ICD9_IMMUNO = [
-
-]
-ICD10_IMMUNO = [
-
-]
+ICD9_IMMUNO = []
+ICD10_IMMUNO = []
 
 
 if __name__ == '__main__':
-    print('Hi :)')
-
     parser = argparse.ArgumentParser(
         description='Basic data parser for MIMIC-IV. Fetches relevant entries of immunosuppresed.'
     )
     #parser.add_argument('dataselect_immuno')
     parser.add_argument('-m', '--mimic', required=True) # Root path of MIMIC. Expects same naming scheme as in Physionet without subtables.and
+    parser.add_argument('-i', '--icd', required=True)   # Path to json file with relevant ICD codes related immuno compromised diseases
 
     args = parser.parse_args()
     PATH_MIMICIV = Path(args.mimic)
+    FPATH_JSON_IMMUNO = Path(args.icd)
 
     # Loads tables as DataFrames
     print('> Loading tables...', end='\r')
@@ -35,4 +33,15 @@ if __name__ == '__main__':
     df_icd = pd.read_csv(PATH_MIMICIV / 'diagnoses_icd.csv.gz')
     df_dicd = pd.read_csv(PATH_MIMICIV / 'd_icd_diagnoses.csv.gz')
 
+    # Load ICD codes
+    with open(FPATH_JSON_IMMUNO) as f:
+        immuno_icds = json.load(f)
+
+        ICD9_IMMUNO = immuno_icds['icd9']
+        ICD10_IMMUNO = immuno_icds['icd10']
+
     #print(' '*100, end='\r')
+
+    print(ICD9_IMMUNO)
+    print()
+    print(ICD10_IMMUNO)
