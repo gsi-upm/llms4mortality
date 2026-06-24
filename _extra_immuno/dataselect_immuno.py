be679827973298fe668ab4aefce39a7a29bc2c5f
Daucco <daucco@gmail.com>
2026-06-24 23:41:43 +0200

    WIP: Data selection for consistency with comparing study.

--- _extra_immuno/dataselect_immuno.py
+++ _extra_immuno/dataselect_immuno.py
@@ -40,8 +40,63 @@ if __name__ == '__main__':
         ICD9_IMMUNO = immuno_icds['icd9']
         ICD10_IMMUNO = immuno_icds['icd10']
 
-    #print(' '*100, end='\r')
 
-    print(ICD9_IMMUNO)
-    print()
-    print(ICD10_IMMUNO)
+    # Selects relevant entries of immunosuppressed ICD codes
+    # Treats ICD9 and 10 separately to avoid collisions
+    df_icd9 = df_icd.loc[df_icd.icd_version == 9]
+    df_icd10 = df_icd.loc[df_icd.icd_version == 10]
+
+    # Fetch HADM_IDs consistent with immunosuppressed ICDs
+
+    # Ensures correct datatype prior to comparison
+
+    df_hadmid_immuno = pd.concat([
+        df_icd9.loc[df_icd9.astype({'icd_code': 'str'}).icd_code.isin(ICD9_IMMUNO)].hadm_id,
+        df_icd10.loc[df_icd10.astype({'icd_code': 'str'}).icd_code.isin(ICD10_IMMUNO)].hadm_id
+    ])
+
+    # Gets unique IDs
+    hadmid_immuno = df_hadmid_immuno.unique()
+
+    ## Apply exclusion criteria
+    # 1. Drops non-first ICU admissions
+
+    # Selects entries related to ICU stays of immunocompromised patients,
+    #   and keeps only the first stay of each admission
+    df_icu_immuno = df_icu.loc[df_icu.hadm_id.isin(hadmid_immuno)]
+
+    count_df = len(df_icu_immuno)
+
+    df_icu_immuno['intime'] = pd.to_datetime(df_icu_immuno['intime'], format='%Y-%m-%d %H:%M:%S')
+    df_icu_immuno = df_icu_immuno.loc[df_icu_immuno.groupby(by=['hadm_id']).intime.idxmin()]
+
+    count_dropped =  count_df - len(df_icu_immuno)
+    print(f'> Dropped {count_dropped} non-first-stay entries...')
+    count_df = len(df_icu_immuno)
+
+    # 2. Drops admissions of patients younger than 18 (at admission time)
+    df_icu_immuno = df_icu_immuno.join(df_patients.set_index('subject_id')[['anchor_age']], on='subject_id', how='inner')
+    df_icu_immuno = df_icu_immuno[df_icu_immuno.anchor_age >= 18]
+
+    # Note: Alternatively, get obfuscated birth year from anchor_year - anchor_age, then compare the result against the year in intime
+    #   to get the patient's age at that specific admission. Doing it with age instead because there's no way of knowing the full birth day.
+    count_dropped =  count_df - len(df_icu_immuno)
+    print(f'> Dropped {count_dropped} entries from young patients...')
+    count_df = len(df_icu_immuno)
+
+
+    # 3. Drops admissions of ICU stays shorter than 6h
+    df_icu_immuno['outtime'] = pd.to_datetime(df_icu_immuno['outtime'], format='%Y-%m-%d %H:%M:%S')
+    df_icu_immuno['stay_length'] = df_icu_immuno['outtime'] - df_icu_immuno['intime']
+    df_icu_immuno = df_icu_immuno.loc[df_icu_immuno.stay_length.dt.total_seconds() >= (6*60*60)]
+    
+    count_dropped = count_df - len(df_icu_immuno)
+    print(f'> Dropped {count_dropped} entries from short stays. {len(df_icu_immuno)} entries remaining.')
+
+    # Randomize and generate sets (train, validation, test / 60, 20, 20)
+    # TODO
+
+    # Export json with adhmission and stay IDs for each set
+    opath = FPATH_JSON_IMMUNO.parent / f'stays_{FPATH_JSON_IMMUNO.name}'
+    with open(opath, 'w') as f:
+        json.dump(df_icu_immuno.stay_id.to_list(), f)
